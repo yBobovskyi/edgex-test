e85ee4faa6742b736d631d80b1d1649465d4336a
Yevhen Bobovskyi <yevhenbobovskyi@gmail.com>
2021-10-04 14:28:09 +0300

    Added Flask web service for monitoring data

---
+++ flask/app/main.py
@@ -0,0 +1,61 @@
+import redis
+import json
+from flask import Flask, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+def cpu_records():
+    r = redis.Redis(host = 'redis', port = 6380, db = 1)
+    out_str = '<table border="1"><tr><th style="width:100px">CPU</th><th>Core 0</th><th style="width:250px">Time</th><th>Core 1</th><th style="width:250px">Time</th><th>Core 2</th><th style="width:250px">Time</th><th>Core 3</th><th style="width:250px">Time</th></tr>'
+    temps = []
+    lock = r.lock('cpu_redis')
+    lock.acquire(blocking = True)
+    for i in range(4):
+        temps.append([])
+        source = 'core' + str(i) + 'temp'
+        for j in range(r.llen(source)):
+            temps[i].append(json.loads(r.lindex(source, j)))
+    lock.release()
+
+    for i in range(len(temps[0])):
+        out_str += '<tr><td></td>'
+        for j in range(len(temps)):
+            out_str += '<td style="text-align:center">' + str(temps[j][i]['value']) + '</td><td style="text-align:center">' + temps[j][i]['origin'] + '</td>'
+        out_str += '</tr>'
+    out_str += '</table>'
+    out_str += '<table><tr><th>Average:</th><th></th></tr>'
+    for i in range(len(temps)):
+        out_str += '<tr><td>Core ' + str(i) + ':</td><td>' + str(sum(float(temp['value']) for temp in temps[i]) / len(temps[i])) + '</td></tr>'
+    out_str += '</table>'
+
+    return out_str
+
+@app.route('/')
+def show_records():
+    str = '<meta http-equiv="refresh" content="5">'
+    str += cpu_records()
+    str += '</meta>'
+    return str
+
+def write_record(record):
+    r = redis.Redis(host = 'redis', port = 6380, db = 1)
+    lock = r.lock('cpu_redis')
+    lock.acquire(blocking = True)
+    r.lpush(record['source'], json.dumps({ 'origin' : record['origin'].strftime("%Y-%m-%d %H:%M:%S"), 'value' : float(record['value']) }))
+    if r.llen(record['source']) > 20:
+        r.rpop(record['source'])
+    lock.release()
+
+@app.route('/data', methods=['POST'])
+def gather_data():
+    data = request.get_json()['readings'][0]
+    record = {
+               'device' : data['deviceName'],
+               'source' : data['resourceName'],
+               'type'   : data['valueType'],
+               'value'  : data['value'],
+               'origin' : datetime.fromtimestamp(int(data['origin']) / 1000000000)
+             }
+    write_record(record)
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
---
+++ mqtt_redis_parser/app/main.py
@@ -0,0 +1,50 @@
+import random
+import paho.mqtt.client as mqtt
+import json
+import redis
+from datetime import datetime
+
+broker = 'edgex-mqtt-broker'
+port = 1883
+topic = '/home/laptop'
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print('Connected to MQTT broker\n');
+    else:
+        print('Failed to connect to MQTT broker, return code %d\n', rc)
+
+def parse_json(json_str):
+    reading = json.loads(json_str)['readings'][0]
+    record = {
+               'device' : reading['deviceName'],
+               'source' : reading['resourceName'],
+               'type'   : reading['valueType'],
+               'value'  : reading['value'],
+               'origin' : datetime.fromtimestamp(int(reading['origin']) / 1000000000)
+             }
+    return record
+
+def write_record(record):
+    r = redis.Redis(host = 'redis', port = 6380, db = 1)
+    lock = r.lock('cpu_redis')
+    lock.acquire(blocking = True)
+    r.lpush(record['source'], json.dumps({ 'origin' : record['origin'].strftime("%Y-%m-%d %H:%M:%S"), 'value' : float(record['value']) }))
+    if r.llen(record['source']) > 20:
+        r.rpop(record['source'])
+    lock.release()
+
+current_record = {}
+
+def on_message(client, userdata, msg):
+    current_record = parse_json(msg.payload.decode())
+    write_record(current_record)
+
+client = mqtt.Client(client_id)
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(broker, port)
+client.subscribe(topic)
+client.loop_forever()
