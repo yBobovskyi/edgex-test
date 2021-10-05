912b15f2cee57e1bae8a19957d0ff21eebf16ed3
Yevhen Bobovskyi <yevhenbobovskyi@gmail.com>
2021-10-05 18:20:14 +0300

    Added eKuiper configuration in start.sh

--- start.sh
+++ start.sh
@@ -10,3 +10,29 @@ docker-compose up -d app-service-rules
 docker-compose up -d rulesengine
 docker-compose up -d switch-mock
 docker-compose up -d device-switch
+
+curl -X POST \
+  http://localhost:59720/streams \
+  -H 'Content-Type: application/json' \
+  -d '{"sql": "create stream demo() WITH (FORMAT=\"JSON\", TYPE=\"edgex\")"}'
+
+curl -X POST \
+  http://localhost:59720/rules \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "id": "rule1",
+  "sql": "SELECT core0temp FROM demo WHERE core0temp > 100.0",
+  "actions": [
+    {
+      "rest": {
+        "url": "http://edgex-core-command:59882/api/v2/device/name/switch/switch",
+        "method": "put",
+        "dataTemplate": "{\"state\":\"OFF\"}",
+        "sendSingle": true
+      }
+    },
+    {
+      "log":{}
+    }
+  ]
+}'
